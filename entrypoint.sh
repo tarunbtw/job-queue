484a582d1653c153ae59bd2a576e5eb8e7feab3c
tarunbtw <tharun30115@gmail.com>
2026-07-05 13:55:20 +0530

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
 
-# Start the worker in the background.
-# If the worker crashes, it stays down; the container keeps running
-# because the server (foreground) is the health signal.
-./worker &
-
-# Run the server in the foreground.
-# The platform will restart the container if the server exits.
+(sleep 2 && ./worker) &
 exec ./server
